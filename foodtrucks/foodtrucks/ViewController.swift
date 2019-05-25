598b4088280054c8bcb9a65d310d7c231ae04b3e
darshan <kmdarshan@gmail.com>
2019-05-25 11:46:44 -0700

    Adding initial files for food trucks projects.

---
+++ foodtrucks/foodtrucks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodtrucks
+//
+//  Created by darshan on 5/25/19.
+//  Copyright © 2019 darshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
